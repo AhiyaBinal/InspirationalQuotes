7920c083a36ca8e22cb6ab6e24809653f5f5bb77
AhiyaBinal <107073852+AhiyaBinal@users.noreply.github.com>
2022-06-20 11:54:24 -0400

    Initial Commit

---
+++ InspirationalQuotes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InspirationalQuotes
+//
+//  Created by Binal Manek on 2022-06-20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ InspirationalQuotesUITests/InspirationalQuotesUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  InspirationalQuotesUITestsLaunchTests.swift
+//  InspirationalQuotesUITests
+//
+//  Created by Binal Manek on 2022-06-20.
+//
+
+import XCTest
+
+class InspirationalQuotesUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
